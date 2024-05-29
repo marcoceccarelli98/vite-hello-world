59cbac093e4bc5248098af8372b08a4c3e19c2f1
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-05-29 16:07:14 +0200

    first commit

---
+++ helloworld/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import AppHero from "./components/AppHero.vue";
+
+export default {
+  name: "MyApp",
+  data() {
+    return {
+      title: "La mia prima app con Vite!",
+    };
+  },
+
+  components: {
+    AppHero,
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <AppHero />
+</template>
+
+<style>
+h1 {
+  color: green;
+}
+</style>
---
+++ helloworld/src/components/AppHero.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: "AppHero",
+  data() {
+    return {
+
+      imageName:"jumbo.png",
+    };
+  },
+
+  methods:{
+    getImagePath: function(img){
+      return new URL(`../assets/${img}`,import.meta.url).href;
+    }
+  }
+};
+</script>
+
+<template>
+  <img :src="getImagePath(imageName)"></img>
+</template>
+
+<style>
+img{
+  width: 100%;
+}
+</style>
